14b86939644a59fd6f4ca88fccfbfb00bf3243fa
Md. Naoshad Hossain <naoshad@Mds-MacBook-Pro.local>
2023-11-28 02:18:43 +0600

    add get opration

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
-
 
 // console.log('0909090', process.env.DB_SECRET_KEY)
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -14,7 +13,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ufduuil.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(process.env.DB_PASS)
+console.log(process.env.DB_PASS);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,25 +29,28 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-  
     const usersCollection = client.db("gymproject").collection("users");
     const classCollection = client.db("gymproject").collection("class");
-    const successstoryCollection = client.db("gymproject").collection("successstory");
-    const subscriberCollection = client.db("gymproject").collection("subscriber");
+    const successstoryCollection = client
+      .db("gymproject")
+      .collection("successstory");
+    const subscriberCollection = client
+      .db("gymproject")
+      .collection("subscriber");
     const trainersCollection = client.db("gymproject").collection("trainers");
-   
+    const weeklyschduleCollection = client.db("gymproject").collection("weeklyschdule");
 
     // mideallwaaere
 
     const verifyToken = (req, res, next) => {
       console.log("inside token", req.headers.authorization);
       if (!req.headers.authorization) {
-        return res.status(401).send({ message: "Unauthorized" });
+        return res.status(401).send({ message: "forbeden access" });
       }
       const token = req.headers.authorization.split(" ")[1];
       jwt.verify(token, process.env.JWT_TOKEN, (err, decoded) => {
         if (err) {
-          return res.status(401).send({ message: "Unauthorized"});
+          return res.status(401).send({ message: "forbidden access" });
         }
 
         req.decoded = decoded;
@@ -58,7 +60,7 @@ async function run() {
 
     const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
-      console.log(email);
+      console.log("01888", email);
       const query = { email: email };
       const users = await usersCollection.findOne(query);
       console.log("55", users);
@@ -73,7 +75,7 @@ async function run() {
 
     app.post("/jwt", async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.DB_JWT_TOKEN, {
+      const token = jwt.sign(user, process.env.JWT_TOKEN, {
         expiresIn: "1h",
       });
       res.send({ token });
@@ -81,7 +83,7 @@ async function run() {
 
     // user info
     // verifyToken,
-    app.get("/users",  async (req, res) => {
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
@@ -103,8 +105,46 @@ async function run() {
       }
     );
 
+    // trainer
+    app.patch(
+      "/users/trainer/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            role: "trainer",
+          },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      }
+    );
+
+    // member
+
+    app.patch(
+      "/users/member/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            role: "member",
+          },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      }
+    );
+
     app.get("/users/admin/:email", async (req, res) => {
       const email = req.params.email;
+
       const query = { email: email };
       const user = await usersCollection.findOne(query);
       let admin = false;
@@ -135,106 +175,54 @@ async function run() {
     });
 
     // classes
-    app.get("/class",  async (req, res) => {
+    app.get("/class", async (req, res) => {
       const result = await classCollection.find().toArray();
       res.send(result);
     });
 
-
     // successstory
 
-     app.get("/successstory",  async (req, res) => {
+    app.get("/successstory", async (req, res) => {
       const result = await successstoryCollection.find().toArray();
       res.send(result);
     });
 
     // trainer
 
-    app.get("/trainers",  async (req, res) => {
+    app.get("/trainers", async (req, res) => {
       const result = await trainersCollection.find().toArray();
       res.send(result);
     });
 
-
-
     app.get("/trainers/:id", async (req, res) => {
       const id = req.params.id;
-      
 
       const query = { _id: new ObjectId(id) };
-    
+
       const result = await trainersCollection.findOne(query);
       res.send(result);
     });
 
+    // subscribe
 
-    // subscribe  
-    
-    app.get("/subscriber",  async (req, res) => {
+    app.get("/subscriber", async (req, res) => {
       const result = await subscriberCollection.find().toArray();
       res.send(result);
     });
-   
 
-    app.post('/subscriber',async(req,res)=>{
+    app.post("/subscriber", async (req, res) => {
       const query = req.body;
       const result = await subscriberCollection.insertOne(query);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
 
-    // payment system
+    // rutin
 
-    // app.post("/create-payment-intent", async (req, res) => {
-    //   const { price } = req.body;
-    //   const amount = parseInt(price * 100);
-    //   console.log("taka", amount);
-
-    //   const paymentIntent = await stripe.paymentIntents.create({
-    //     amount: amount,
-    //     currency: "usd",
-    //     payment_method_types: ["card"],
-    //   });
-
-    //   res.send({
-    //     clientSecret: paymentIntent.client_secret,
-    //   });
-    // });
-
-    // // payments
-
-    // app.get("/payments/:email", verifyToken, async (req, res) => {
-    //   const query = { email: req.params.email };
-
-    //   if (req.params.email !== req.decoded.email) {
-    //     return res.status(403).send({ message: "fobeden" });
-    //   }
-
-    //   const result = await paymentCollection.find(query).toArray();
-    //   res.send(result);
-    // });
-
-    // app.post("/payments", async (req, res) => {
-    //   const payment = req.body;
-    //   const paymentResult = await paymentCollection.insertOne(payment);
-
-    //   // lalalala
-    //   console.log("info", payment);
-    //   const query = {
-    //     _id: {
-    //       $in: payment.cartIds.map((id) => new ObjectId(id)),
-    //     },
-    //   };
-    //   const deleteResult = await cartsCollection.deleteMany(query);
-    //   res.send({ paymentResult, deleteResult });
-    // });
-
-    
-  
-
-
-
-
+    app.get("/weeklyschedule", async (req, res) => {
+      const result = await weeklyschduleCollection.find().toArray();
+      res.send(result);
+    });
 
     // r=end
 
