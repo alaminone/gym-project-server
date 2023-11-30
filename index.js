394bffd39d92106d3ca0c53dce0025bfda643d01
Md. Naoshad Hossain <naoshad@Mds-MacBook-Pro.local>
2023-11-30 10:44:52 +0600

    added vercel.json

--- index.js
+++ index.js
@@ -320,10 +320,10 @@ async function run() {
 
     // r=end
 
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
